7f96f116f77d9599eaffe907c02710a9812483cd
Gustavo Medina <medinagustavo45@gmail.com>
2021-02-15 19:38:41 -0600

    Creación del archivo 'read.php'

---
+++ api/task/read.php
@@ -0,0 +1,44 @@
+<?php
+
+/** Añadir headers */
+header('Access-Controll-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Connection.php';
+include_once '../../models/Task.php';
+
+/** Establecer connecion a la DB */
+
+$connection = new Connection();
+$db = $connection->connect();
+
+/** Creación de 'task' */
+
+$task = new Task($db);
+$result = $task->getTask();
+
+$rows = $result->rowCount();
+
+/** Verificar si existen Tasks */
+if ($rows > 0) {
+    $task_array = array();
+    $task_array['data'] = array();
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $task_item = array(
+            'id_task' => $id_task,
+            'description' => $description,
+            'done' => $done
+        );
+
+        array_push($task_array['data'], $task_item);
+    }
+
+    echo json_encode($task_array);
+} else {
+    echo json_encode(
+        array('message' => 'No tasks')
+    );
+}
--- models/Task.php
+++ models/Task.php
@@ -1,10 +1,28 @@
 <?php
 
-class Task{
+class Task
+{
     private $conn;
     private $table = 'tasks';
 
     public $id;
     public $description;
     public $status;
+
+    /** Constructor */
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getTask()
+    {
+        $query = "SELECT *
+                    FROM {$this->table}";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
