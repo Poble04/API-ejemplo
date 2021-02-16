f2765b2696199fcc88eb30e051f4d9187040810d
Gustavo Medina <medinagustavo45@gmail.com>
2021-02-15 20:03:21 -0600

    Creación de la función 'getSingleTask' para tomar 1 solo elemento del recurso

--- models/Task.php
+++ models/Task.php
@@ -25,4 +25,24 @@ class Task
 
         return $stmt;
     }
+
+    public function getSingleTask()
+    {
+        $query = "SELECT *
+                    FROM {$this->table}
+                    WHERE id_task = ?";
+
+        $stmt = $this->conn->prepare($query);
+
+        /** Bind id_task */
+        $stmt->bindParam(1, $this->id_task);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->description = $row['description'];
+        $this->done = $row['done'];
+
+        return $stmt;
+    }
 }
